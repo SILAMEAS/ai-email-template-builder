48558540095ab5282946d35c57dd4fed83a31a26
Sila MEAS <sila.meas@allweb.com.kh>
2025-01-29 17:45:26 +0700

    click-img-home

--- components/custom/editor/left-side/elements/components/SocialMediaElements.tsx
+++ components/custom/editor/left-side/elements/components/SocialMediaElements.tsx
@@ -1,18 +1,22 @@
-import React from 'react';
-import {ElementListInterface} from "@/Data/ElementLists";
+import React from "react";
+import { ElementListInterface } from "@/Data/ElementLists";
 import ImageElement from "@/components/custom/editor/left-side/elements/components/ImageElement";
 import Link from "next/link";
 
-const SocialMediaElements = ({socialIcons,outerStyle,style}:Pick<ElementListInterface, "socialIcons"|"outerStyle"|"style">) => {
-    return (
-        <div style={outerStyle}>
-            {
-                socialIcons?.map(item=><Link key={item.icon} href={item.url}>
-                    <ImageElement imageUrl={item.icon} style={style}/>
-                </Link>)
-            }
-        </div>
-    );
+const SocialMediaElements = ({
+  socialIcons,
+  outerStyle,
+  style,
+}: Pick<ElementListInterface, "socialIcons" | "outerStyle" | "style">) => {
+  return (
+    <div style={outerStyle}>
+      {socialIcons?.map((item) => (
+        <Link key={item.icon} href={item.url}>
+          <ImageElement imageUrl={item.icon} style={style} />
+        </Link>
+      ))}
+    </div>
+  );
 };
 
 export default SocialMediaElements;
