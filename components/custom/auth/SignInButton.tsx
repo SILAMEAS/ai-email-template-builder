5ad5e34e773cdbd88e58b9d0e8e84b6353c10e2a
Sila MEAS <sila.meas@allweb.com.kh>
2025-01-29 12:12:56 +0700

    SignInButton-refactor

--- components/custom/auth/SignInButton.tsx
+++ components/custom/auth/SignInButton.tsx
@@ -9,29 +9,35 @@ import { api } from "@/convex/_generated/api";
 const SignInButton = () => {
   const CreateUser = useMutation(api.users.CreateUser);
   const googleLogin = useGoogleLogin({
-    onSuccess: async (tokenResponse) => {
-      const userInfo = await axios.get(
-        "https://www.googleapis.com/oauth2/v3/userinfo",
-        { headers: { Authorization: "Bearer " + tokenResponse?.access_token } },
-      );
-      const user = userInfo?.data;
-      /** =========================================== */
-      /**              Save to database              */
-      /** =========================================== */
-      if (user) {
-        const result = await CreateUser({
-          name: user.name,
-          email: user.email,
-          picture: user.picture,
-          credit: 3,
+    onSuccess: (tokenResponse) => {
+      axios
+        .get("https://www.googleapis.com/oauth2/v3/userinfo", {
+          headers: { Authorization: "Bearer " + tokenResponse?.access_token },
+        })
+        .then((r) => {
+          const user = r.data;
+          if (user) {
+            /** =========================================== */
+            /**              Save to database              */
+            /** =========================================== */
+            CreateUser({
+              name: user.name,
+              email: user.email,
+              picture: user.picture,
+              credit: 3,
+            }).then((result) => {
+              if (window) {
+                localStorage.setItem(
+                  "userDetail",
+                  JSON.stringify({
+                    ...user,
+                    _id: (result as any)?.id ?? result,
+                  }),
+                );
+              }
+            });
+          }
         });
-        if (window) {
-          localStorage.setItem(
-            "userDetail",
-            JSON.stringify({ ...user, _id: (result as any)?.id ?? result }),
-          );
-        }
-      }
     },
     onError: (errorResponse) => console.log(errorResponse),
   });
